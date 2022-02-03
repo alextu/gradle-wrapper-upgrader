682a7984d57fb94928322abd2019a40e5a79249a
Alexis Tual <atual@gradle.com>
2022-02-03 10:52:54 +0100

    Cleanin up

--- buildSrc/src/main/groovy/GitPush.groovy
+++ buildSrc/src/main/groovy/GitPush.groovy
@@ -1,10 +1,8 @@
 import org.gradle.api.DefaultTask
-import org.gradle.api.model.ObjectFactory
 import org.gradle.api.provider.Property
 import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.file.FileCollection
 import org.gradle.api.tasks.Input
-import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
 import org.gradle.process.ExecOperations
 import org.gradle.work.DisableCachingByDefault
